ba19d25fd1c7e684102d027f3dc6c601d3cae370
u-lu <urban.lutz@outlook.com>
2024-06-01 13:30:20 +0200

    Add Experiment Class

--- src/experiment_runner.py
+++ src/experiment_runner.py
@@ -2,11 +2,33 @@ from datetime import datetime
 from typing import Callable
 from tqdm import tqdm
 from src.dataset import TDMSDataset, PATH, LogResult
+from src.models import Model
+
+class Experiment:
+    """
+    model: A Model from src.models
+    prompt_template: A function that inserts the tex segment into a prompt
+    extract: a function taking a model, a prompt template and a tex file, defining how the model with the prompt tempalte gets applied to the tex file
+    """
+    def __init__(
+            self, 
+            model: Model,
+            prompt_template: Callable[[str], str],
+            extract: Callable[[Model, Callable[[str], str], str], str],
+            name: str
+            ):
+        self.model = model
+        self.extract = extract
+        self.name = name
+        self.prompt_template = prompt_template
+    
+    def __call__(self, sample: str) -> str:
+        return self.extract(self.model, self.prompt_template, sample)
+
 
 def run(
-        extract_fn: Callable[[str], str],
+        experiment: Experiment,
         data_path: PATH,
-        run_name:str,
         max_iter = None
         ):
     """
@@ -15,7 +37,7 @@ def run(
     """
     # define run name
     now_str = datetime.now().strftime('%Y%m%d-%H%M%S')
-    run_id = f"{PATH.get_name(data_path)}_{run_name}-{now_str}"
+    run_id = f"{PATH.get_name(data_path)}_{experiment.name}-{now_str}"
 
 
     dataset = TDMSDataset(data_path)
@@ -26,8 +48,38 @@ def run(
     indexes = len(dataset)
     for i in tqdm(range(indexes)):
         f, tex, ground_truth = dataset[i]
-        model_output = extract_fn(tex)
+        model_output = experiment(tex)
 
         logger.log(f, str(model_output), str(ground_truth))
     df = logger.save()
     return df
+
+
+# def run(
+#         extract_fn: Callable[[str], str],
+#         data_path: PATH,
+#         run_name:str,
+#         max_iter = None
+#         ):
+#     """
+#     extract_fn: A function passing the text to a language model, augmented by a prompt, to extract TDMS quadruples
+#     run_name: Information about the run, model name, experiment setup etc
+#     """
+#     # define run name
+#     now_str = datetime.now().strftime('%Y%m%d-%H%M%S')
+#     run_id = f"{PATH.get_name(data_path)}_{run_name}-{now_str}"
+
+
+#     dataset = TDMSDataset(data_path)
+#     if max_iter:
+#         dataset.all_paths = dataset.all_paths[:max_iter]
+#     logger = LogResult(run_id, do_write=True, additional_col_names=["ground_truth"])
+
+#     indexes = len(dataset)
+#     for i in tqdm(range(indexes)):
+#         f, tex, ground_truth = dataset[i]
+#         model_output = extract_fn(tex)
+
+#         logger.log(f, str(model_output), str(ground_truth))
+#     df = logger.save()
+#     return df
---
+++ src/prompt_templates.py
@@ -0,0 +1,124 @@
+import json
+
+from src.dataset import UNANSWERABLE
+
+
+# prompt template
+def extract_tdms_initial(tex, few_shot=True):
+    if few_shot:
+        return f"""If the text reports benchmark leaderboard results, extract the reported Tasks, Datasets, Metrics and corresponding Scores.
+        
+        Text: {tex}
+        
+        Return the tasks, datasets, metrics and scores as reported in the text in a JSON array. Do not include precision information in the reported score.
+        Here the formating structure of the JSON. Please use exactly this formating in your answer.
+        [
+            {{"Task": "example Task 1", "Dataset": "example Dataset 1", "Metric": example metric 1", "Score": "score"}}, 
+            {{"Task": "example Task 1", "Dataset": "example Dataset 2", "Metric": example metric 2", "Score": "score"}}
+        ]
+        
+        Lets make an example for you: Template-Based Automatic Search of Compact Semantic Segmentation Architectures... One discovered architecture achieves 63.2% mean IoU on CamVid and 67.8% on CityScapes having only 270K parameters... evaluation.
+        
+        The expected answer of you is:
+        [
+            {{"Task": "Compact Sementic Segmentation", "Dataset": "CamVid", "Metric": Mean IoU", "Score": "63.2"}}, 
+            {{"Task": "Compact Sementic Segmentation", "Dataset": "CityScapes", "Metric": Mean IoU", "Score": "67.8"}}
+        ]
+        
+        """
+    else:
+        return f"""If the text reports benchmark leaderboard results, extract the reported Tasks, Datasets, Metrics and corresponding Scores.
+        
+        Text: {tex}
+        
+        Return the tasks, datasets, metrics and scores as reported in the text in a JSON array. Do not include precision information in the reported score.
+        Here the formating structure of the JSON. Please use exactly this formating in your answer.
+        [
+            {{"Task": "example Task 1", "Dataset": "example Dataset 1", "Metric": example metric 1", "Score": "score"}}, 
+            {{"Task": "example Task 1", "Dataset": "example Dataset 2", "Metric": example metric 2", "Score": "score"}}
+        ]
+        
+        """
+    
+def extract_tdms_optimized01(tex, few_shot=True):
+    if few_shot:
+        return f"""I am a researcher and I want you to extract benchmark leaderboard results from the provided sequence of a scholarly article about Artificial Intelligence. Please return a JSON object with the defined structure below. I expect you to find TDMS(task, dataset, metric, score) tuples. Do not include precision information in the reported score. Please have a look at the example and the expected result below as well. Thank you.
+
+        JSON structure:
+        [
+            {{"Task": "example Task 1", "Dataset": "example Dataset 1", "Metric": example metric 1", "Score": "score"}}, 
+            {{"Task": "example Task 1", "Dataset": "example Dataset 2", "Metric": example metric 2", "Score": "score"}}
+        ]
+        
+        Example:
+        Template-Based Automatic Search of Compact Semantic Segmentation Architectures... One discovered architecture achieves 63.2% mean IoU on CamVid and 67.8% on CityScapes having only 270K parameters... evaluation.
+        
+        Result:
+        [
+            {{"Task": "Compact Sementic Segmentation", "Dataset": "CamVid", "Metric": Mean IoU", "Score": "63.2"}}, 
+            {{"Task": "Compact Sementic Segmentation", "Dataset": "CityScapes", "Metric": Mean IoU", "Score": "67.8"}}
+        ]
+        
+        Sequence: 
+        {tex}
+        
+        """
+    else:
+        return f"""I am a researcher and I want you to extract benchmark leaderboard results from the provided sequence of a scholarly article about Artificial Intelligence. Please return a JSON object with the defined structure below. I expect you to find TDMS(task, dataset, metric, score) tuples. Do not include precision information in the reported score. Thank you.
+
+        JSON structure:
+        [
+            {{"Task": "example Task 1", "Dataset": "example Dataset 1", "Metric": example metric 1", "Score": "score"}}, 
+            {{"Task": "example Task 1", "Dataset": "example Dataset 2", "Metric": example metric 2", "Score": "score"}}
+        ]
+        
+        
+        Sequence: 
+        {tex}
+        
+        """
+    
+def extract_tdms_optimized02(tex, few_shot=True):
+    if few_shot:
+        return f"""I am a researcher and I want you to extract benchmark leaderboard results from the provided sequence of a scholarly article about Artificial Intelligence. Please return a JSON object with the defined structure below. I expect you to find TDMS(task, dataset, metric, score) tuples. Most common tasks are image classification, atari games, node classification, object detection, video retrieval, link prediction, semantic segmentation, semi-supervised video object segmentation, 3d human pose estimation and question answering. Most common datasets are imagenet, coco test-dev, human3.6m, cifar-10, coco minival, youtube-vos 2018, cifar-100, msr-vtt-1ka, fb15k-237 and msu super-resolution for video compression. Most common metrics are accuracy, score, f1, psnr, map, miou, ssim, top 1 accuracy, 1:1 accuracy and number of params. Do not include precision information in the reported score. Please have a look at the example and the expected result below as well. Thank you.
+
+        JSON structure:
+        [
+            {{"Task": "example Task 1", "Dataset": "example Dataset 1", "Metric": example metric 1", "Score": "score"}}, 
+            {{"Task": "example Task 1", "Dataset": "example Dataset 2", "Metric": example metric 2", "Score": "score"}}
+        ]
+        
+        Example:
+        Template-Based Automatic Search of Compact Semantic Segmentation Architectures... One discovered architecture achieves 63.2% mean IoU on CamVid and 67.8% on CityScapes having only 270K parameters... evaluation.
+        
+        Result:
+        [
+            {{"Task": "Compact Sementic Segmentation", "Dataset": "CamVid", "Metric": Mean IoU", "Score": "63.2"}}, 
+            {{"Task": "Compact Sementic Segmentation", "Dataset": "CityScapes", "Metric": Mean IoU", "Score": "67.8"}}
+        ]
+        
+        Sequence: 
+        {tex}
+        
+        """
+    else:
+        return f"""I am a researcher and I want you to extract benchmark leaderboard results from the provided sequence of a scholarly article about Artificial Intelligence. Please return a JSON object with the defined structure below. I expect you to find TDMS(task, dataset, metric, score) tuples. Most common tasks are image classification, atari games, node classification, object detection, video retrieval, link prediction, semantic segmentation, semi-supervised video object segmentation, 3d human pose estimation and question answering. Most common datasets are imagenet, coco test-dev, human3.6m, cifar-10, coco minival, youtube-vos 2018, cifar-100, msr-vtt-1ka, fb15k-237 and msu super-resolution for video compression. Most common metrics are accuracy, score, f1, psnr, map, miou, ssim, top 1 accuracy, 1:1 accuracy and number of params. Do not include precision information in the reported score. Thank you.
+
+        JSON structure:
+        [
+            {{"Task": "example Task 1", "Dataset": "example Dataset 1", "Metric": example metric 1", "Score": "score"}}, 
+            {{"Task": "example Task 1", "Dataset": "example Dataset 2", "Metric": example metric 2", "Score": "score"}}
+        ]
+        
+        Sequence: 
+        {tex}
+        
+        """
+
+
+zero_shot_template_initial = lambda tex: extract_tdms_initial(tex, False)
+few_shot_template_initial = lambda tex: extract_tdms_initial(tex, True)
+zero_shot_template_optimized01 = lambda tex: extract_tdms_optimized01(tex, False)
+few_shot_template_optimized01 = lambda tex: extract_tdms_optimized01(tex, True)
+zero_shot_template_optimized02 = lambda tex: extract_tdms_optimized02(tex, False)
+few_shot_template_optimized02 = lambda tex: extract_tdms_optimized02(tex, True)
