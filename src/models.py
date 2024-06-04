ce38793ce9d7a3e547c6f8e5a3de96f4c02b447a
u-lu <urban.lutz@outlook.com>
2024-06-04 08:47:07 +0200

    update parse output

--- src/models.py
+++ src/models.py
@@ -1,9 +1,41 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import ollama
 
+
+def _convert_tdms_to_tuple(model_output_parsed):
+    tuples = []
+    for item in model_output_parsed:
+        try:
+            t = ((item["Task"], item["Dataset"],item["Metric"],item["Score"]))
+            tuples.append(t)
+        except:
+            # parse error, ignore instance
+            pass
+    return tuples
+
+def _format_tdms(tuples):
+    """make unique, format as string"""
+    unique = set(tuples)
+    dicts = [{"LEADERBOARD": {
+        "Task": t,
+        "Dataset":d,
+        "Metric":m,
+        "Score":s
+    }} for t,d,m,s in unique]
+    return str(dicts)
+
 class Model:
     def generate(self, prompt: str)-> str:
         pass
+
+    def parse_response(self, response):
+        try:
+            response = "[" + res.split("[", 1)[-1].rsplit("]", 1)[0] + "]"
+            response = json.loads(response)
+            response = _convert_tdms_to_tuple(response)
+            return _format_tdms(response)
+        except:
+            return str(response)
 
 class OllamaModel(Model):
     def __init__(self, model):
@@ -13,7 +45,7 @@ class OllamaModel(Model):
         try:
             res = ollama.generate(model=self.model, prompt=prompt, options={"temperature": 0})
             res = res["response"]
-            res = "[" + res.split("[", 1)[-1].rsplit("]", 1)[0] + "]"
+            res = self.parse_response(res)
             return res
         except Exception as ex:
             print(ex)
