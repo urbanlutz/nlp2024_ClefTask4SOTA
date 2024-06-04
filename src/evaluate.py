a5f032b1afcbf3082b5c9d2981deb434252547f1
u-lu <urban.lutz@outlook.com>
2024-06-04 14:46:41 +0200

    prompt evals

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,6 +1,6 @@
 # evaluate
-from scoring_program.evaluation import Metrics
 import pandas as pd
+from scoring_program.evaluation import Metrics
 
 def evaluate(df):
     results = Metrics.evaluate_property_wise_json_based(
@@ -10,4 +10,4 @@ def evaluate(df):
     results.update(
         Metrics.evaluate_rouge(label_list=list(df["ground_truth"]), prediction_list=list(df["annotation"]))
     )
-    return pd.DataFrame([results]).T
+    return pd.DataFrame([results])
