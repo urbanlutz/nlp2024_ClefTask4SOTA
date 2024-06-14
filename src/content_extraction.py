89e0f3aa48dc5fc4bb92aa53987af8423080d88c
u-lu <urban.lutz@outlook.com>
2024-06-14 15:10:49 +0200

    update

--- src/content_extraction.py
+++ src/content_extraction.py
@@ -109,6 +109,7 @@ def _regex_demonstration_away(text):
     r = r'\{[\"|\']LEADERBOARD[\"|\']: ?\{[\"|\']Task[\"|\']: ?[\"|\']Facial Expression Recognition \(?FER\)?[\"|\'], ?[\"|\']Dataset[\"|\']: ?[\"|\']Oulu-CASIA[\"|\'], ?[\"|\']Metric[\"|\']: ?[\"|\']Accuracy ?\(?\-? ?(10\-fold|weak expression|peak expression|combined)\)?[\"|\'], ?[\"|\']Score[\"|\']: [\"|\']?(\d\d).(\d\d)[\"|\']\}\}'
     return re.sub(r, "", text)
     
+    
 def _clean_unparsable_chars(text):
     return re.sub(r"\.\.\.", "", text)
 
